e237e0c30563828130b5d14105a29081d3e95d5b
Peter <peterhsaxton@gmail.com>
2014-08-14 08:43:57 +0100

    refactored tests

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 describe('dot', function () {
-    var dot;
+    var dot, person;
 
     beforeEach(function () {
         dot = require('./index.js');
+        person = {name: 'Mike'};
     });
 
-    it('should pull an objects value', function () {
-        var person = {name: 'Mike'};
+    it('should pull an objects value, curried', function () {
         var name = dot('name');
         expect(name(person)).toEqual('Mike');
     });
